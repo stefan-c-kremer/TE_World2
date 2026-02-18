efee85bd03b42964b6642c8c5813ed9d51c3a2d8
isincl01 <isincl01@uoguelph.ca>
2026-02-18 16:16:02 -0500

    Update Lambda Function

--- TE-Experiments/ISExp002/parameters.py
+++ TE-Experiments/ISExp002/parameters.py
@@ -57,7 +57,7 @@ Append_gene = True;	# True: when the intialization routine tries to place
 
 Initial_TEs = 30;
 Autonomous_Frequency = 0.6 # 60% of the elements are autonomous (i.e. are LINE)
-Kidnapping_Frequency = lambda live_aut: 1 - 1/(1 + 0.1 * live_aut)  # The larger the number, the higher the probability of non-autonomous kidnapping the autonomous TEs
+Kidnapping_Frequency = lambda live_aut, live_naut: 1 - 1/(1 + 0.1 * live_naut)  # The larger the number, the higher the probability of non-autonomous kidnapping the autonomous TEs
 
 MILLION = 1000000;
 
