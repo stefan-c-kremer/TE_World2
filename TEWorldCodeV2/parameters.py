c059b831520d42e0b9e8bdf7879502d490924f6e
Isss11 <isaiah.sinclair.0711@gmail.com>
2026-04-09 15:51:02 -0400

    Implement Revised Parameters

--- TEWorldCodeV2/parameters.py
+++ TEWorldCodeV2/parameters.py
@@ -83,8 +83,8 @@ Host_reproduction_rate = 1;  # how many offspring each host has
 Host_survival_rate = lambda propfit: min( Carrying_capacity * propfit, 0.95 );
     # propfit = proportion of fitness owned by this individual
     
-Initial_TEs = 1;
-Total_NAut_TE = int(Initial_TEs * Carrying_capacity * 3)
+Initial_Aut_TEs = 1;
+Initial_NAut_TEs = 3
 
 Maximum_generations = 50;
 Terminate_no_TEs = True;	# end simulation if there are no TEs left
@@ -97,10 +97,6 @@ save_frequency = 50;	# Frequency with with which to save state of experiment
 
 saved = None;	# if saved = None then we start a new simulation from scratch
 		# if saves = string, then we open that file and resume a simulation
-  
-  
-# Total Non-autonomous TE
-Total_NAut_TE = int(Initial_TEs * Carrying_capacity * 3)
 
 # Kidnapping Frequency
 Kidnapping_Frequency = lambda live_aut, live_naut : 1 - 1/(1 + 0.07 * live_naut)
