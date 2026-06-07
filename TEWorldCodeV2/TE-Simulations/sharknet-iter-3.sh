5b53c9e8cf700867bfcf0a2c1e37e9488a40d714
Isss11 <isaiah.sinclair.0711@gmail.com>
2026-06-07 14:08:27 -0400

    Add Sharknet Files

---
+++ TEWorldCodeV2/TE-Simulations/sharknet-iter-3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --time=4-00:00 # D-HH:MM
+#SBATCH --account=def-skremer
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=48
+module load ipython-kernel/3.10
+
+# Re-runs simulation #1 only for experiments that have less than 300 generations
+python3 RunSimulations.py -s -r 3 -g 300
